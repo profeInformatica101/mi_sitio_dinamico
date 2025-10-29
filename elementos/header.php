1676faf6950fa8c38d1ddb13a24d111442d82f18
Pablo <jdommay863@g.educaand.es>
2025-10-29 09:37:38 +0100

    menu

--- elementos/header.php
+++ elementos/header.php
@@ -1,4 +1,25 @@
+<?php
+/**
+ * Menu
+ */
+$menu = [
+  'inicio' => 'Inicio',
+  'contenido' => 'Productos',
+  'contacto' => 'Contacto'
+];
+
+?>
 <header class="text-center bg-white p-3 rounded shadow-sm">
   <h1 class="text-primary">🌟 Bienvenido a mi primer sitio PHP 🌟</h1>
   <p class="text-muted">Usando include() por primera vez</p>
+<ul class="nav nav-pills">
+  <?php foreach ($menu as $clave => $texto): ?>
+    <li class="nav-item">
+      <a class="nav-link <?= ($p === $clave) ? 'active' : '' ?>"
+         href="index.php?p=<?= $clave ?>">
+         <?= htmlspecialchars($texto) ?>
+      </a>
+    </li>
+  <?php endforeach; ?>
+</ul>
 </header>
